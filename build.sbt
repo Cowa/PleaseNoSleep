8361156d95229a2ca0b9512664d6fee3f364512a
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-11 21:42:52 +0100

    Implement a ping scheduler

--- build.sbt
+++ build.sbt
@@ -6,15 +6,7 @@ scalaVersion := "2.11.5"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.3.9",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.9",
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-  "junit" % "junit" % "4.12" % "test",
-  "com.novocode" % "junit-interface" % "0.11" % "test",
   "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
-  "com.enragedginger" %% "akka-quartz-scheduler" % "1.3.0-akka-2.3.x"
+  "org.slf4j" % "slf4j-simple" % "1.7.10",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
 )
-
-testOptions += Tests.Argument(TestFrameworks.JUnit, "-v")
-
-
-fork in run := true
--- src/main/scala/PleaseNoSleep.scala
+++ src/main/scala/PleaseNoSleep.scala
@@ -1,12 +1,35 @@
-import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
-import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Inbox }
-import scala.concurrent.duration._
 import dispatch._
 import Defaults._
+import scala.util.Properties
+import akka.actor.ActorSystem
+import org.slf4j.LoggerFactory
+import scala.concurrent.duration._
+import com.typesafe.scalalogging._
 
 object PleaseNoSleep extends App {
-  val system = ActorSystem("PleaseNoSleep")
-  val scheduler = QuartzSchedulerExtension(system)
+  val system = ActorSystem("system")
+  val logger = Logger(LoggerFactory.getLogger("PleaseNoSleep"))
+  val urlToPing: List[String] = Properties.envOrElse("URL_TO_PING", "").split(",").toList
 
-  println("ok")
+  system.scheduler.schedule(0 seconds, 5 minutes){(
+    pingAll(urlToPing)
+  )}
+
+  /** Ping all URL to keep them awake
+    *
+    * @param urls List of every URL to ping
+    */
+  def pingAll(urls: List[String]): Unit = urls match {
+    case h :: q => ping(h); pingAll(q)
+    case Nil => Unit
+  }
+
+  /** Ping an URL to keep him awake
+    *
+    * @param toPing URL to ping
+    */
+  def ping(toPing: String): Unit = {
+    val response = Http(url(toPing) OK as.String)
+    logger.info("Ping " + toPing)
+  }
 }

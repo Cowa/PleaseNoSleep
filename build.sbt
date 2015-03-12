5cc883f0ec4de9f30fd7b4a5876823d8736086a9
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-12 22:34:50 +0100

    Small HTTP server to deploy on Heroku

--- build.sbt
+++ build.sbt
@@ -7,9 +7,14 @@ scalaVersion := "2.11.5"
 lazy val root = (project in file("."))
   .enablePlugins(JavaServerAppPackaging)
 
+resolvers += "spray repo" at "http://repo.spray.io"
+
 libraryDependencies ++= Seq(
   "org.slf4j" % "slf4j-simple" % "1.7.10",
   "com.typesafe.akka" %% "akka-actor" % "2.3.9",
   "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
-  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+  "io.spray" %% "spray-servlet" % "1.3.2",
+  "io.spray" %% "spray-routing" % "1.3.2",
+  "io.spray" %% "spray-can" % "1.3.2"
 )
--- src/main/scala/PleaseNoSleep.scala
+++ src/main/scala/PleaseNoSleep.scala
@@ -1,17 +1,24 @@
 import dispatch._
 import Defaults._
+import akka.io.IO
+import spray.can.{Http => HttpS}
 import scala.util.Properties
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import org.slf4j.LoggerFactory
 import scala.concurrent.duration._
 import com.typesafe.scalalogging._
+import spray.util._
+import spray.http._
 
 object PleaseNoSleep extends App {
-  val system = ActorSystem("system")
+  implicit val system = ActorSystem("system")
   val logger = Logger(LoggerFactory.getLogger("PleaseNoSleep"))
   val urlToPing: List[String] = Properties.envOrElse("URL_TO_PING", "").split(",").toList
 
-  system.scheduler.schedule(0 seconds, 5 minutes){(
+  val handler = system.actorOf(Props[Handler], name = "handler")
+  IO(HttpS) ! HttpS.Bind(handler, "localhost", 8080)
+
+  system.scheduler.schedule(0 seconds, 5 seconds){(
     pingAll(urlToPing)
   )}
 
@@ -31,5 +38,12 @@ object PleaseNoSleep extends App {
   def ping(toPing: String): Unit = {
     val response = Http(url(toPing) OK as.String)
     logger.info("Ping " + toPing)
+  }
+}
+
+class Handler extends Actor {
+  def receive = {
+    case _: HttpS.Connected => sender ! HttpS.Register(self)
+    case _: HttpRequest => sender ! HttpResponse(entity = "Please, no sleep.")
   }
 }
